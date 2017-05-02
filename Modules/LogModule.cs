8282b33504a01c3d5ddb5821063f992092d20e6a
MateuszByra <mat.byra@gmail.com>
2017-05-02 15:09:07 +0200

    Add new web forms project, new library with LogModule which contains PreApplicationStartMethod. Refernce LogModule to mvc and web forms projects.

---
+++ Modules/LogModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+[assembly: PreApplicationStartMethod(typeof(Modules.LogModule), "Register")]
+
+namespace Modules
+{
+    public class LogModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            Debug.WriteLine("Dispose was logged!");
+        }
+
+        public void Init(HttpApplication context)
+        {
+            Debug.WriteLine("Init was logged!");
+        }
+
+        public static void Register()
+        {
+            Debug.WriteLine("PreApplicationStartMethod Register()");
+        }
+    }
+}
